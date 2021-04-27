8a4aae318a318bc57bef8a12af75c49cd2c6f165
edwardsun007 <edwardsun007@gmail.com>
2021-04-26 23:07:49 -0700

    Added event query logic and structjson util from dialogflow v2 client

--- backend/chatbot/chatbot.js
+++ backend/chatbot/chatbot.js
@@ -1,5 +1,6 @@
 'use strict'
 const dialogflow = require('dialogflow');
+const structjson = require('./structjson');
 const config = require('../config/keys');
 
 const sessionClient = new dialogflow.SessionsClient();
@@ -35,6 +36,24 @@ module.exports = {
       console.error(err);
       return null;
     }
+  },
+
+  eventQuery: async function(event, parameters={}) {
+    let self = module.exports; // get ref to this module so we can use other methods here
+    const request = {
+      session: sessionPath,
+      queryInput: {
+        event: {
+          name: event,
+          parameters: structjson.structProtoToJson(parameters),
+          languageCode: config.dialogFlowSessionLanguageCode,
+        },
+      }
+    };
+
+    let responses = await sessionClient.detectIntent(request);
+    responses = await self.handleAction(responses);
+    return responses;
   },
 
   handleAction: function(responses){
--- backend/routes/dialogFlowRoutes.js
+++ backend/routes/dialogFlowRoutes.js
@@ -1,6 +1,7 @@
 const chatbot = require('../chatbot/chatbot');
 
 module.exports = app => {
+  // query by text input from UI
   app.post('/api/df_text_query', async (req, res)=>{
     let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
     if (responses){
@@ -14,7 +15,25 @@ module.exports = app => {
             console.log(`  No intent matched.`);
       }
     } else {
-      res.send('Error: something went wrong');
+      res.send('Error: something went wrong with text query');
+    }
+  });
+
+  // query by event
+  app.post('/api/df_event_query', async (req, res)=>{
+    let responses = await chatbot.eventQuery(req.body.event, req.body.parameters);
+    if (responses){
+      const result = responses[0].queryResult;
+      console.log(`  Query: ${result.queryText}`);
+      console.log(`  Response: ${result.fulfillmentText}`);
+      res.send(result);
+      if (result.intent) {
+            console.log(`  Intent: ${result.intent.displayName}`);
+      } else {
+            console.log(`  No intent matched.`);
+      }
+    } else {
+      res.send('Error: something went wrong with event query');
     }
   });
   
@@ -23,7 +42,5 @@ module.exports = app => {
     res.send('user id with book id route');
   });
 
-  app.post('/api/df_event_query', (req, res)=>{
-    res.send({'do': 'event query'})
-  });
+
 }
