84138e2a9ec5fb2d3d5cc43441b4d5f7fed9fba6
edwardsun007 <edwardsun007@gmail.com>
2021-04-27 00:16:31 -0700

    Adding env vars to Heroku and key configs

--- backend/chatbot/chatbot.js
+++ backend/chatbot/chatbot.js
@@ -3,7 +3,14 @@ const dialogflow = require('dialogflow');
 const structjson = require('./structjson');
 const config = require('../config/keys');
 
-const sessionClient = new dialogflow.SessionsClient();
+const projectID = config.googleProjectID;
+const credentials = {
+  client_email: config.googleClientEmail,
+  private_key: config.googlePrivateKey
+};
+
+
+const sessionClient = new dialogflow.SessionsClient({projectID: projectID, credentials: credentials});
 
 const sessionPath = sessionClient.sessionPath(config.googleProjectID, config.dialogFlowSessionID);
 
---
+++ backend/config/prod.js
@@ -0,0 +1,8 @@
+/** DO NOT COMMIT THIS FILE */
+module.exports = {
+  googleProjectID: process.env.GOOGLE_PROJECT_ID,
+  dialogFlowSessionID: process.env.DIALOGFLOW_SESSION_ID,
+  dialogFlowSessionLanguageCode: process.env.DIALOGFLOW_SESSION_LANGCODE,
+  googleClientEmail: process.env.GOOGLE_CLIENT_EMAIL,
+  googlePrivateKey: process.env.GOOGLE_PRIVATE_KEY
+}
